b3b2b085be59b8b64504e85b0f7fc52309c99310
Tobias Mattern <me@tobc.de>
2025-09-28 20:01:48 +0200

    Initial Commit

---
+++ copyPasta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  copyPasta
+//
+//  Created by Tobias Mattern on 28.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ copyPasta/copyPastaApp.swift
@@ -0,0 +1,17 @@
+//
+//  copyPastaApp.swift
+//  copyPasta
+//
+//  Created by Tobias Mattern on 28.09.25.
+//
+
+import SwiftUI
+
+@main
+struct copyPastaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
